7af325474f4730dbe6e8239b9823535c4f2ad8e0
lojoeugenio4-max <lojoeugenio4@gmail.com>
2026-06-20 11:37:23 +0200

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://bohlxagrtpjvqrgkonlo.supabase.co";
-const supabaseAnonKey = "AQUI_PEGARAS_LA_PUBLISHABLE_KEY";
+const supabaseAnonKey = sb_publishable_tpgtppDeMr2dGJIiZtB5nA_OXih8FKF;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
